348ffcffc27f0b39aa57b8a3fa53f6327ea527d4
Dmitry Kashinsky <dmitry.kashinsky@sparkequation.com>
2021-05-05 17:46:20 +0400

    add material-ui, cleanup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import { Button, Dialog, DialogContent, DialogActions } from '@material-ui/core';
+import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
 import './App.css';
 
+const useStyles = makeStyles((theme) =>
+  createStyles({
+    appButton: {
+      '&:not(:last-child)': {
+        marginRight: theme.spacing(2),
+      }
+    },
+  }),
+);
+
 function App() {
+  const { appButton } = useStyles();
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Button className={appButton} variant="contained" color="primary" onClick={handleOpen}>
+          Edit Controlling Org
+        </Button>
+        <Button className={appButton} variant="contained" color="secondary" onClick={handleOpen}>
+          Edit Deal
+        </Button>
       </header>
+      <Dialog open={open} onClose={handleClose} aria-labelledby="form-dialog-title">
+        <DialogContent>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="secondary">
+            Cancel
+          </Button>
+          <Button onClick={handleClose} color="primary">
+            Save
+          </Button>
+        </DialogActions>
+      </Dialog>
     </div>
   );
 }
